a5bbbabcf092cc4102393f6e512201c3809876ef
Dipto K Kundu <dkkundu00@gmail.com>
2022-11-12 01:46:56 +0600

    add file

--- ai_ml_system/views/pnenumonia_train_model.py
+++ ai_ml_system/views/pnenumonia_train_model.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
-import pandas as pd
-import keras
-import matplotlib.pyplot as plt
+# import tensorflow as tf
+# from keras.preprocessing.image import ImageDataGenerator
+# import pandas as pd
+# import keras
+# import matplotlib.pyplot as plt
 
 
 def train_pneumonia_disease_model(request):
